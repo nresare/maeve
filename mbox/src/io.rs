f2544dbe559ca7b1c6afa4546fbf1afa2a15270f
Noa Resare <noa@resare.com>
2023-08-27 18:51:24 +0100

    wip: implemented short read handling

---
+++ mbox/src/io.rs
@@ -0,0 +1,89 @@
+#[cfg(test)]
+use anyhow::Result;
+#[cfg(test)]
+use std::io::Read;
+
+/// A variant of Reader.read() that will handle short reads by repeating the read until
+/// there is a zero read.
+#[cfg(test)]
+pub fn read_handling_short(mut reader: impl Read, buf: &mut [u8]) -> Result<usize> {
+    let mut this_count = reader.read(buf)?;
+    let mut total_count = this_count;
+    while this_count > 0 && total_count < buf.len() {
+        this_count = reader.read(&mut buf[total_count..])?;
+        total_count += this_count;
+    }
+    Ok(total_count)
+}
+
+#[cfg(test)]
+mod test {
+    macro_rules! r {
+        ( $x:expr ) => {
+            Cursor::new(&$x[..])
+        };
+    }
+
+    use crate::io::read_handling_short;
+    use anyhow::Result;
+    use std::io::{Cursor, Read};
+    use std::ops::Deref;
+    #[test]
+    fn read_data_larger_than_buf() -> Result<()> {
+        let mut reader = r!(b"abcdefghijklmnop");
+
+        let mut buf = [0u8; 2];
+        let count = read_handling_short(&mut reader, &mut buf[..])?;
+        assert_eq!(b"ab"[..], buf[..2]);
+        assert_eq!(2, count);
+
+        let count = read_handling_short(&mut reader, &mut buf[..])?;
+        assert_eq!(b"cd"[..], buf[..2]);
+        assert_eq!(2, count);
+        Ok(())
+    }
+
+    #[test]
+    fn read_data_smaller_than_buf() -> Result<()> {
+        let mut reader = r!(b"abcd");
+
+        let mut buf = [0u8; 6];
+        let count = read_handling_short(&mut reader, &mut buf)?;
+        assert_eq!(4, count);
+        Ok(())
+    }
+
+    struct ShortReader {
+        to_write: Vec<Box<[u8]>>,
+    }
+
+    impl Read for ShortReader {
+        fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+            match self.to_write.pop() {
+                None => Ok(0),
+                Some(data) => {
+                    if buf.len() > data.len() {
+                        buf = &mut buf[..data.len()];
+                    }
+                    buf.copy_from_slice(data.deref());
+                    Ok(data.len())
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn short_read() -> Result<()> {
+        let mut data = vec![Box::from(&b"abc"[..]), Box::from(&b"def"[..])];
+        data.reverse();
+        let mut s = ShortReader { to_write: data };
+        let mut buf = [0u8; 6];
+        let count = read_handling_short(&mut s, buf.as_mut())?;
+        assert_eq!(&b"abcdef"[..], buf.as_ref());
+        assert_eq!(6, count);
+        let count = read_handling_short(&mut s, buf.as_mut())?;
+        assert_eq!(0, count);
+
+        Ok(())
+    }
+}
--- mbox/src/lib.rs
+++ mbox/src/lib.rs
@@ -1,34 +1,41 @@
-use std::io::Read;
-use anyhow::Result;
+mod io;
 
+#[cfg(test)]
+use crate::io::read_handling_short;
+#[cfg(test)]
+use anyhow::Result;
+#[cfg(test)]
+use bytes::BytesMut;
+#[cfg(test)]
+use std::io::Read;
+#[cfg(test)]
 const BUF_SIZE: usize = 8192;
 
+#[cfg(test)]
 struct Parser {
     reader: Box<dyn Read>,
-    buf: Option<[u8; BUF_SIZE]>,
-    start: usize,
+    buf: BytesMut,
+    //start: usize,
 }
 
+#[cfg(test)]
 impl Parser {
     fn new(reader: Box<dyn Read>) -> Self {
-        Parser{reader, buf: None, start: 0}
+        Parser {
+            reader,
+            buf: BytesMut::zeroed(BUF_SIZE),
+        }
     }
 
     fn get_lines(&mut self) -> Result<Vec<String>> {
-        let buf = self.buf.get_or_insert_with(|| [0u8; BUF_SIZE]);
-        let count = self.reader.read(&mut buf[self.start..])?;
+        let count = read_handling_short(&mut self.reader, self.buf.as_mut())?;
         let mut result: Vec<String> = Vec::new();
         let mut start = 0;
-        for (i, c) in (buf[0..count]).iter().enumerate() {
+        for (i, c) in (self.buf[0..count]).iter().enumerate() {
             if *c == 0x0a {
-                result.push(String::from_utf8(buf[start..i].to_vec())?);
+                result.push(String::from_utf8(self.buf[start..i].to_vec())?);
                 start = i + 1;
             }
-        }
-        if start < count && start > 0 {
-            let to_copy = (&buf[start..count]).clone();
-            buf.copy_from_slice(to_copy);
-            self.start = to_copy.len();
         }
         Ok(result)
     }
@@ -40,23 +47,23 @@ mod tests {
 
     #[test]
     fn test_read() -> Result<()> {
-        let mut parser = Parser::new(Box::new(b"foo\nbar\nbaz".as_ref()));
+        let mut parser = Parser::new(Box::new(b"foo\nbar\nbaz\n".as_ref()));
         let result = parser.get_lines()?;
         assert_eq!(vec!["foo", "bar", "baz"], result);
         Ok(())
     }
 
-    #[test]
-    fn test_lines_from_file() -> Result<()> {
-        let file = std::fs::File::open("/Users/nresare/slask/vox")?;
-        let mut parser = Parser::new(Box::new(file));
-        loop {
-            let lines = parser.get_lines()?;
-            if lines.len() == 0 {
-                break;
-            }
-            println!("{:?}", lines.len());
-        }
-        Ok(())
-    }
+    // #[test]
+    // fn test_lines_from_file() -> Result<()> {
+    //     let file = std::fs::File::open("kafka")?;
+    //     let mut parser = Parser::new(Box::new(file));
+    //     loop {
+    //         let lines = parser.get_lines()?;
+    //         if lines.len() == 0 {
+    //             break;
+    //         }
+    //         println!("{:?}", lines.len());
+    //     }
+    //     Ok(())
+    // }
 }
